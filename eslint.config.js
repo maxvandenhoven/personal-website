08e3de56cc263342fc86873ef91b6fb9f42d290c
Max van den Hoven <max.hoven@gmail.com>
2026-08-02 21:00:05 +0200

    chore: ensure pre-commit catches all formatting/linting issues

--- eslint.config.js
+++ eslint.config.js
@@ -6,26 +6,32 @@ import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [{
-  ignores: ["dist/", ".astro/", "node_modules/"],
-}, {
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    parser: tsparser,
-    parserOptions: {
-      ecmaFeatures: { jsx: true },
+export default [
+  {
+    ignores: ["dist/", ".astro/", "node_modules/"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   },
-  plugins: {
-    "@typescript-eslint": tseslint,
-    "react-hooks": reactHooks,
-  },
-  rules: {
-    ...tseslint.configs.recommended.rules,
-    ...reactHooks.configs.recommended.rules,
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_" },
-    ],
-  },
-}, ...eslintPluginAstro.configs.recommended, eslintConfigPrettier, ...storybook.configs["flat/recommended"]];
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+  ...storybook.configs["flat/recommended"],
+];
